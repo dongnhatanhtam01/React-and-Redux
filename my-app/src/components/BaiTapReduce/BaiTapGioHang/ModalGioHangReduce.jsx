106aff95a5d652cb6bea0f7a24aa0b7f468ae645
tamdna <tamdna@vietmap.vn>
2020-12-14 00:40:40 +0700

    tangGiamSL

--- my-app/src/components/BaiTapReduce/BaiTapGioHang/ModalGioHangReduce.jsx
+++ my-app/src/components/BaiTapReduce/BaiTapGioHang/ModalGioHangReduce.jsx
@@ -1,70 +1,116 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class ModalGioHangReduce extends Component {
-    renderGioHang = () => {
-        return this.props.gioHang.map((item, index)=>{
-            return (
-                <tr key={index}>
-                    <td>{item.maSP}</td>
-                    <td>{item.tenSP}</td>
-                    <td><img src={item.hinhAnh} alt="adsasd" width={85} height={125}/></td>
-                    <td>{item.giaBan}</td>
-                    <td>{item.soLuong}</td>
-                    <td>{(item.soLuong*item.giaBan).toLocaleString()}</td>
-                    <td><button className="btn btn-danger" onClick={()=> this.props.DeleteCartFunc(index)}>Xóa</button></td>
-                    <td><button className="btn btn-danger" onClick={()=> this.props.DeleteCodeFunc(item.maSP)}>Xóa = Mã</button></td>
-                </tr>
-            )
-        })
-    }
-    render() {
-        //goi thu props moi
+  renderGioHang = () => {
+    return this.props.gioHang.map((item, index) => {
+      return (
+        <tr key={index}>
+          <td>{item.maSP}</td>
+          <td>{item.tenSP}</td>
+          <td>
+            <img src={item.hinhAnh} alt="adsasd" width={85} height={125} />
+          </td>
+          <td>{item.giaBan}</td>
+          <td>
+            <button
+              onClick={() => this.props.UpDown(index, false)}
+              className="btn btn-success"
+            >
+              -
+            </button>
+            {item.soLuong}
+            <button
+              onClick={() => this.props.UpDown(index, true)}
+              className="btn btn-success"
+            >
+              +
+            </button>
+          </td>
+          <td>{(item.soLuong * item.giaBan).toLocaleString()}</td>
+          <td>
+            <button
+              className="btn btn-danger"
+              onClick={() => this.props.DeleteCartFunc(index)}
+            >
+              Xóa
+            </button>
+          </td>
+          <td>
+            <button
+              className="btn btn-danger"
+              onClick={() => this.props.DeleteCodeFunc(item.maSP)}
+            >
+              Xóa = Mã
+            </button>
+          </td>
+        </tr>
+      );
+    });
+  };
+  render() {
+    //goi thu props moi
 
-        return (
-            <div className="container">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Prods Code</th>
-                            <th>Prods Name</th>
-                            <th>Image</th>
-                            <th>Units</th>
-                            <th>Number</th>
-                            <th>Cost</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderGioHang()}
-                        
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
+    return (
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Prods Code</th>
+              <th>Prods Name</th>
+              <th>Image</th>
+              <th>Units</th>
+              <th>Number</th>
+              <th>Cost</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderGioHang()}</tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="5"></td>
+              <td>Tổng tiền: </td>
+              <td>
+                {this.props.gioHang.reduce((tongTien, item, index) => {
+                  return (tongTien += item.soLuong * item.giaBan);
+                }, 0)}
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    );
+  }
 }
-const mapStateToProps = (state) => {// chuyen state tren store thanh this.props.gioHang
-    return {
-        gioHang: state.CartReducer.gioHang
-    }
-}
+const mapStateToProps = (state) => {
+  // chuyen state tren store thanh this.props.gioHang
+  return {
+    gioHang: state.CartReducer.gioHang,
+  };
+};
 const mapDispatchToProps = (dispatch) => {
-    return  {
-        DeleteCartFunc: (index) => {
-            const action= {
-                type: 'XOA_GIO_HANG',
-                index
-            }
-            dispatch(action);
-        },
-        DeleteCodeFunc: (maSP) => {
-            const action={
-                type: "XOA_GIO_HANG_SP",
-                maSP
-            }
-            dispatch(action)
-        }
-    }
-}
-export default connect(mapStateToProps,mapDispatchToProps)(ModalGioHangReduce)
+  return {
+    UpDown: (index, tangGiam) => {
+      const action = {
+        type: "TANG_GIAM_SL",
+        index,
+        tangGiam,
+      };
+      dispatch(action);
+    },
+    DeleteCartFunc: (index) => {
+      const action = {
+        type: "XOA_GIO_HANG",
+        index,
+      };
+      dispatch(action);
+    },
+    DeleteCodeFunc: (maSP) => {
+      const action = {
+        type: "XOA_GIO_HANG_SP",
+        maSP,
+      };
+      dispatch(action);
+    },
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(ModalGioHangReduce);
--- my-app/src/redux/reducers/CartReducer.jsx
+++ my-app/src/redux/reducers/CartReducer.jsx
@@ -12,37 +12,52 @@ const stateGioHang = {
 
 export const CartReducer = (state = stateGioHang, action) => {
   console.log("reducer", action);
-  switch(action.type){
-    case 'XOA_GIO_HANG_SP': {
+  switch (action.type) {
+    case "TANG_GIAM_SL": {
+      const { index, tangGiam } = action;
+      console.log(tangGiam);
       let gioHangCapNhat = [...state.gioHang];
-      let index = gioHangCapNhat.findIndex(item => item.maSP === action.maSP);
-      if(index!==-1) {
-        gioHangCapNhat.splice(index,1);
-      }
-     
-      // render lai giao dien
-      state.gioHang = gioHangCapNhat;
-      return {...state}
-    }
-    case 'XOA_GIO_HANG': {
-      let gioHangCapNhat = [...state.gioHang];
-      gioHangCapNhat.splice(action.index,1);
-      // render lai giao dien
-      state.gioHang = gioHangCapNhat;
-      return {...state}
-    }
-    case 'ADD_TO_CART': {
-      let gioHangCapNhat  = [...state.gioHang];
-      let index = gioHangCapNhat.findIndex(item => item.maSP === action.nhanspGioHang.maSP);
-      if(index!==-1){
-        gioHangCapNhat[index].soLuong +=1
-      }else {
-        gioHangCapNhat.push(action.nhanspGioHang)
+      if (tangGiam) {
+        gioHangCapNhat[index].soLuong += 1;
+      } else {
+        if (gioHangCapNhat[index].soLuong > 1) {
+          gioHangCapNhat[index].soLuong -= 1;
+        }
       }
       state.gioHang = gioHangCapNhat;
-      return {...state};
+      return { ...state };
     }
-    
+    case "XOA_GIO_HANG_SP": {
+      let gioHangCapNhat = [...state.gioHang];
+      let index = gioHangCapNhat.findIndex((item) => item.maSP === action.maSP);
+      if (index !== -1) {
+        gioHangCapNhat.splice(index, 1);
+      }
+
+      // render lai giao dien
+      state.gioHang = gioHangCapNhat;
+      return { ...state };
+    }
+    case "XOA_GIO_HANG": {
+      let gioHangCapNhat = [...state.gioHang];
+      gioHangCapNhat.splice(action.index, 1);
+      // render lai giao dien
+      state.gioHang = gioHangCapNhat;
+      return { ...state };
+    }
+    case "ADD_TO_CART": {
+      let gioHangCapNhat = [...state.gioHang];
+      let index = gioHangCapNhat.findIndex(
+        (item) => item.maSP === action.nhanspGioHang.maSP
+      );
+      if (index !== -1) {
+        gioHangCapNhat[index].soLuong += 1;
+      } else {
+        gioHangCapNhat.push(action.nhanspGioHang);
+      }
+      state.gioHang = gioHangCapNhat;
+      return { ...state };
+    }
   }
-  return {...state}
+  return { ...state };
 };
